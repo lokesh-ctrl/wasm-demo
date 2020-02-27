f01a9a0a7cba315733e90cf530e8890c6688e482
Lokesh Rongali <lokesh@everest.engineering>
2020-02-27 19:49:46 +0530

    Remove unnecessary file

--- src/index.js
+++
@@ -1,6 +0,0 @@
-import loadWasm from './lib.rs'
-
-loadWasm().then(result => {
-    const fibonocci_number = result.instance.exports['fibonocci_number'];
-    console.log('return value was', fibonocci_number(32));
-});
